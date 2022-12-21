4b5c67c14ba9a0761009dd6ecf30e300abdf1aac
Zach Rosol <roso5868@kettering.edu>
2022-12-20 20:40:47 -0500

    Engine Block Framework Built

---
+++ code/slave/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: pcguy
+ *
+ * Created on December 20, 2022, 2:27 AM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
